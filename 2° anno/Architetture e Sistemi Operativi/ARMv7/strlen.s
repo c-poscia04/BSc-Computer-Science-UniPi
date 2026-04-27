b8dd7a72e6b73c427729a6c4b03fb0aa3dc88653
Camilla Poscia <camillaposcia6@gmail.com>
2026-04-27 16:16:23 +0200

    Update strlen.s

--- 2° anno/Architetture e Sistemi Operativi/ARMv7/strlen.s
+++ 2° anno/Architetture e Sistemi Operativi/ARMv7/strlen.s
@@ -4,11 +4,11 @@
 strlen:
     MOV R1, #0          ; Inizializza il contatore (lunghezza) a 0
 loop:
-    LDRB R2, [R0], #1   ; Carica il byte puntato da R0 in R2 e incrementa R0 di 1 [10]
-    CMP R2, #0          ; Verifica se il carattere è il terminatore NULL (0) [6]
-    BEQ fine            ; Se è 0, salta alla fine del ciclo [11]
+    LDRB R2, [R0], #1   ; Carica il byte puntato da R0 in R2 e incrementa R0 di 1 
+    CMP R2, #0          ; Verifica se il carattere è il terminatore NULL (0) 
+    BEQ fine            ; Se è 0, salta alla fine del ciclo 
     ADD R1, R1, #1      ; Altrimenti, incrementa il contatore della lunghezza
     B loop              ; Ripete il ciclo
 fine:
-    MOV R0, R1          ; Mette il risultato (lunghezza) in R0 per il ritorno [1]
-    MOV PC, LR          ; Ritorna al chiamante [7, 8]
+    MOV R0, R1          ; Mette il risultato (lunghezza) in R0 per il ritorno 
+    MOV PC, LR          ; Ritorna al chiamante
