92a3b591befb57ada11b3aafca57b81228535fbc
Camilla Poscia <camillaposcia6@gmail.com>
2026-08-03 13:08:38 +0200

    Update fun.s

--- 2° anno/Architetture e Sistemi Operativi/ARMv7/fun.s
+++ 2° anno/Architetture e Sistemi Operativi/ARMv7/fun.s
@@ -1,7 +1,13 @@
-.global fun
+.text
+	.global fun
+	
 fun:
-    LSL R1, R0, #1      ; R1 = 2x
-    ADD R1, R1, R0      ; R1 = 2x + x = 3x
-    ADD R1, R1, #1      ; R1 = 3x + 1
-    ASR R0, R1, #2      ; R0 = (3x + 1) / 4 (risultato in R0 per il ritorno)
-    MOV PC, LR          ; Ritorna al chiamante
+	// @f(x) = (3x+1)/4
+	// Parametro 'x' ricevuto in r0
+	
+	lsl r1, r0, #1      // r1 = x * 2 (spostamento a sinistra di 1 bit = moltiplicazione per 2)
+	add r1, r1, r0      // r1 = (2 * x) + x = 3x
+	add r1, r1, #1      // r1 = 3x + 1
+	asr r0, r1, #2      // r0 = (3x + 1) / 4 (spostamento a destra aritmetico di 2 bit = divisione per 4)
+	
+	mov pc, lr          // Ritorna al chiamante (salto all'indirizzo in LR)
