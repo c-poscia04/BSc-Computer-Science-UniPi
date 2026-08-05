870e5d2fb6d1a5db4291edbbda2f5a658669766f
Camilla Poscia <camillaposcia6@gmail.com>
2026-08-05 02:34:22 +0200

    Update fact.s

--- 2° anno/Architetture e Sistemi Operativi/ARMv7/fact.s
+++ 2° anno/Architetture e Sistemi Operativi/ARMv7/fact.s
@@ -1,24 +1,22 @@
 .global fact
 .type fact, %function
-.text
 
-; Calcola n! - R0 riceve n
-fact:
-    CMP r0, #0          ; Caso base: n == 0?
-    BGT continue        ; Se n > 0, vai a 'continue'
+fact: 
+    cmp r0, #0
+    beq base            ; Se r0 == 0, salta al caso base
+
+    @ --- CASO RICORSIVO ---
+    push {r0, lr}       ; Salva sullo stack n (in r0) e l'indirizzo di ritorno (lr)
+    sub r0, r0, #1      ; Calcola n - 1
+    bl fact             ; Chiama ricorsivamente fact(n-1). Il risultato ora è in r0.
     
-   ; Se n <= 0, il fattoriale e' 1
-    mov r0, #1          ; Carica 1 come risultato
+    @ Adesso dobbiamo recuperare il vecchio 'n' dallo stack e moltiplicarlo per il risultato (in r0)
+    mov r1, r0          ; Sposta temporaneamente il risultato di fact(n-1) in r1
+    pop {r0, lr}        ; Ripristina il vecchio 'n' in r0 (e il vecchio lr)
+    
+    mul r0, r0, r1      ; Calcola n * fact(n-1) e mette il risultato in r0
     mov pc, lr          ; Ritorna al chiamante
 
-continue:
-    push {r0, lr}       ; Salva n (r0) e l'indirizzo di ritorno (lr) nello stack
-    sub r0, r0, #1      ; Prepara n-1 per la chiamata ricorsiva
-    
-    bl fact       ; Chiamata ricorsiva: calcola (n-1)!
-    
-    ; Al ritorno: R0 contiene il risultato di (n-1)!
-    pop {r1, lr}        ; Recupera il vecchio n (in r1) e il vecchio lr
-    
-    mul r0, r0, r1      ; R0 = (n-1)! * n
-    mov pc, lr          ; Ritorna con il risultato finale in R0
+base: 
+    mov r0, #1          ; Caso base: se n == 0, restituisce 1
+    mov pc, lr          ; Ritorna al chiamante
