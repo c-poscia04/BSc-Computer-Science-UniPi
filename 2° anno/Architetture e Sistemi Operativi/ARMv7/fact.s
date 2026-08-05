53d92b5846a40757fd3a649da94f1b756ceb88b0
Camilla Poscia <camillaposcia6@gmail.com>
2026-08-05 02:34:40 +0200

    Update fact.s

--- 2° anno/Architetture e Sistemi Operativi/ARMv7/fact.s
+++ 2° anno/Architetture e Sistemi Operativi/ARMv7/fact.s
@@ -5,12 +5,12 @@ fact:
     cmp r0, #0
     beq base            ; Se r0 == 0, salta al caso base
 
-    @ --- CASO RICORSIVO ---
+    ; --- CASO RICORSIVO ---
     push {r0, lr}       ; Salva sullo stack n (in r0) e l'indirizzo di ritorno (lr)
     sub r0, r0, #1      ; Calcola n - 1
     bl fact             ; Chiama ricorsivamente fact(n-1). Il risultato ora è in r0.
     
-    @ Adesso dobbiamo recuperare il vecchio 'n' dallo stack e moltiplicarlo per il risultato (in r0)
+    ; Adesso dobbiamo recuperare il vecchio 'n' dallo stack e moltiplicarlo per il risultato (in r0)
     mov r1, r0          ; Sposta temporaneamente il risultato di fact(n-1) in r1
     pop {r0, lr}        ; Ripristina il vecchio 'n' in r0 (e il vecchio lr)
     
